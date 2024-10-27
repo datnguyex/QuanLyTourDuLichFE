238bfc9ae2d94b24e83c9dacfdd2c040c410fad5
datnguyex <nnguyendat72@gmail.com>
2024-10-27 19:44:52 +0700

    update addTourToFavorite interface 27/10/2024

--- src/Page/Home/Home.vue
+++ src/Page/Home/Home.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-      <h1>Home Page</h1>
       <div v-if="displayErrors">{{ displayErrors }}</div>
       <b-container v-if="displayErrors == null">
         <b-row>
             <b-col v-for="(tour, index) in newesTour" :key="index" cols="3">
             <div>
               <b-card
+           
                 :title="tour.name"
                 img-src="https://picsum.photos/600/300/?image=25"
                 img-alt="Image"
@@ -18,7 +18,8 @@
                 <b-card-text>
                   {{ tour.description }}
                 </b-card-text>
-                <b-button variant="primary">Go somewhere</b-button>
+                <b-button @click="handleAddTourToFavorite(tour.id)" variant="primary">Add to Favorite</b-button>
+                <!-- <b-button @click="logValue" variant="primary">log</b-button> -->
               </b-card>
             </div>
           </b-col>
@@ -31,10 +32,12 @@
   <script>
   import axios from 'axios';
   import { onMounted, ref } from 'vue';
-  
+  import { inject } from 'vue';
+
   export default {
     name: 'HomeComponent',
     setup() {
+      const valueCurrentUser = inject('valueCurrentUser');
       const newesTour = ref([]);
        const displayErrors = ref(null);
       const getNewesTour = async () => {
@@ -48,12 +51,30 @@
           console.log('displayErrors.value',displayErrors.value);
         }
       };
-  
+      const handleAddTourToFavorite = async (tour_id) => {
+    try {
+        const response = await axios.post('http://localhost:8000/api/addTourToFavorite', {
+            'user_id': valueCurrentUser.value.id,
+            'tour_id': tour_id,
+        });
+      alert(response.data.message);
+    } catch (error) {
+        if (error.response) {
+            console.error('Failed to retrieve tours:', error.response.data);
+            alert(error.response.data.message)
+        } else {
+            console.error('Error:', error.message);
+        }
+    }
+};
+  const logValue = () => {
+    console.log('valueCurrentUser',valueCurrentUser);
+  }
       onMounted(() => {
         getNewesTour();
       });
   
-      return { newesTour,displayErrors };
+      return { newesTour,displayErrors,handleAddTourToFavorite,logValue };
     }
   }
   </script>
